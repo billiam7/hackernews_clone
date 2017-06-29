c5ec81a1444aa2de0d48b16c549bf47cc0c46d52
Bill Lapko <wlapko@gmail.com>
2017-06-29 16:45:17 -0400

    push to heroku

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,6 +1,6 @@
 class SubmissionsController < ApplicationController
   before_action :set_submission, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
+  before_filter :authenticate_user!, except: [:index, :show]
 
   # GET /submissions
   # GET /submissions.json
